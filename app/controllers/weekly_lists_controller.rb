c3cd502d0b81d7385607195812cbbf298a9a5e53
Liz R <lizr588@gmail.com>
2018-08-02 18:01:21 -0500

    this week lists added

--- app/controllers/weekly_lists_controller.rb
+++ app/controllers/weekly_lists_controller.rb
@@ -1,7 +1,17 @@
 class WeeklyListsController < ApplicationController
   def this_week
+    this_week = ThisWeek.create(recipe_id: params[:recipe_id])
+  end
+
+  def view_week
+    @this_weeks = ThisWeek.all
   end
 
   def shopping_list
+    
+  end
+
+  def shopping
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'thisweek' => 'weekly_lists#this_week'
-  get 'shoppinglist' => 'weekly_lists#shopping_list'
+  post 'thisweek' => 'weekly_lists#this_week'
+  get 'mealplan' => 'weekly_lists#view_week'
+  post 'shoppinglist' => 'weekly_lists#shopping_list'
+  get 'shopping' => 'weekly_lists#shopping'
   get 'main/index'
   get :search, controller: :main
   resources :store_maps
