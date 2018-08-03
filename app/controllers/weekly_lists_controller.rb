d7a7f1d3b08576853a3804adbf97ca0d7418b3e3
Liz R <lizr588@gmail.com>
2018-08-03 08:19:50 -0500

    no change

--- app/controllers/weekly_lists_controller.rb
+++ app/controllers/weekly_lists_controller.rb
@@ -1,17 +1,22 @@
 class WeeklyListsController < ApplicationController
+# push item to this week
   def this_week
     this_week = ThisWeek.create(recipe_id: params[:recipe_id])
   end
 
+# view week's recipes
   def view_week
     @this_weeks = ThisWeek.all
   end
 
+# push ingredients to shopping list
   def shopping_list
     shopping_list = ShoppingList.create(recipe_id: params[:recipe_id], ingredient_id: params[:ingredient_id])
   end
 
+# view shopping list
   def shopping
     @shopping_lists = ShoppingList.all
+    @ingredients = Ingredients.all
   end
 end
